9b3be9b4c015071e37a012ee1de2416fc595f6b0
neil patel <neil@myindianweddingapp.com>
2014-04-05 19:47:22 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinteresting::Application.config.secret_key_base = '24025c0f21da2be11dbe934e193b196038a4bc27b562106b19c211144c7adde439bba1431b72e4dcd6dacf298a752d4de33a90e3ab714b34e58024cd60fa9db5'
