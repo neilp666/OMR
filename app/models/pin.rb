898226a888d61a06893c09559221fdcd79632724
neil patel <neil@myindianweddingapp.com>
2014-04-09 20:52:11 +0100

    Add names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,8 @@ class Pin < ActiveRecord::Base
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+
+	 validates :image, presence: true
+  	 validates :description, presence: true
 end
